57060e7742ae5b52fe9f46007c5460e9e6a5acc6
kain <kain@teamclerks.net>
2024-01-11 14:29:55 -0800

    Okay, I think we're ready for styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,5 @@ import db from '@/helpers/db';
 export default async function Home() {
   const posts = await db.getPosts();
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Markdown markdown={posts[0].body} />
-    </main>
-  );
+  return <Markdown markdown={posts[0].body} />;
 }
