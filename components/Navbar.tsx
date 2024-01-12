10ae7451fa60ff70c2fdeb8c6da55626fef8a580
kain <kain@teamclerks.net>
2024-01-12 15:01:53 -0800

    I don't understand suspense

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,4 @@
-import { Skeleton } from '@mantine/core';
-import { Suspense, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import slugs from '@/helpers/slugs';
 import { Post } from '@/helpers/db';
 import Link from 'next/link';
@@ -18,43 +17,30 @@ export default function NavBar({ toggle }: { toggle: () => void }) {
   return (
     <>
       <h3>Recent Posts</h3>
-      <Suspense
-        fallback={Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={true}>
-              Test
-            </Skeleton>
-          ))}
-      >
-        <ul>
-          {recentPosts.map((post) => (
-            <li
-              key={slugs.slugify(post.title)}
-              className="flex flew-row justify-between"
-            >
-              <Link
-                href={`/post/${slugs.slugify(post.title)}`}
-                onClick={toggle}
-              >
-                {post.title}
-              </Link>
-              <time
-                dateTime={
-                  post.updated_at
-                    ? new Date(post.updated_at).toLocaleDateString()
-                    : new Date(post.created_at).toLocaleDateString()
-                }
-                className="italic text-sm"
-              >
-                {post.updated_at
+      <ul>
+        {recentPosts.map((post) => (
+          <li
+            key={slugs.slugify(post.title)}
+            className="flex flew-row justify-between"
+          >
+            <Link href={`/post/${slugs.slugify(post.title)}`} onClick={toggle}>
+              {post.title}
+            </Link>
+            <time
+              dateTime={
+                post.updated_at
                   ? new Date(post.updated_at).toLocaleDateString()
-                  : new Date(post.created_at).toLocaleDateString()}
-              </time>
-            </li>
-          ))}
-        </ul>
-      </Suspense>
+                  : new Date(post.created_at).toLocaleDateString()
+              }
+              className="italic text-sm"
+            >
+              {post.updated_at
+                ? new Date(post.updated_at).toLocaleDateString()
+                : new Date(post.created_at).toLocaleDateString()}
+            </time>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
