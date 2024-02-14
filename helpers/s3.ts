1fb837a49e3d3d698083511d3fe343268bdcac78
kain <kain@teamclerks.net>
2024-02-14 10:44:14 -0800

    Fixed upload type

--- helpers/s3.ts
+++ helpers/s3.ts
@@ -1,19 +1,16 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { fromEnv } from '@aws-sdk/credential-provider-env';
 
 async function uploadPublicImage(file: File) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   const client = new S3Client({
     region: 'us-east-1',
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
+    credentials: fromEnv(),
   });
   const command = new PutObjectCommand({
     Bucket: 'blog-teamclerks-net-images',
     Key: file.name,
     Body: Buffer.from(await file.arrayBuffer()),
+    ContentType: file.type,
   });
 
   await client.send(command);
